18c0df03f2873614a5d272d18da407784ff185ba
PartiedOutPhil <kylemrhickson@gmail.com>
2022-04-19 17:48:00 -0400

    Revert "Added toggleButton"
    This reverts commit 63b33edf2aec1aff5b12038417577d960ffda5d3.

--- helloWorld/Source/MainComponent.h
+++ helloWorld/Source/MainComponent.h
@@ -1,4 +1,4 @@
- #pragma once
+#pragma once
 
 #include <JuceHeader.h>
 
@@ -21,7 +21,6 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
-    juce::ToggleButton togglebutton;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
